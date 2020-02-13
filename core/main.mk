9ec8637eb9d0b97516b91bb06cfdf13e6566ae7c
sdv22 <sdv22@live.ru>
2020-02-13 13:21:21 +0300

    Not ready yet
    Change-Id: I8301ec7bd2c430cabf79dc28bae23386f23e7bc0

--- core/main.mk
+++ core/main.mk
@@ -370,7 +370,7 @@ ADDITIONAL_BUILD_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/aicp/build/core/main.mk
+# -include vendor/aicp/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
